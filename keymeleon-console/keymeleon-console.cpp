86dfeeb9654a70c03c79194457494f7cf34a1dee
Jack Gillespie <jack02gillespie@gmail.com>
2022-08-08 18:31:52 +0100

    Refactored. Full 105-key support

--- keymeleon-console/keymeleon-console.cpp
+++ keymeleon-console/keymeleon-console.cpp
@@ -8,17 +8,36 @@
 int main()
 {
 	// Initialize the hidapi library
-	int res = hid_init();
+	hid_init();
 
+	// this small section shows that inactive profiles can be altered
+	char a;
+
+	setActiveProfile(3);
+	std::cin >> a;
+	
+	std::array<uint8_t, 3> colours = {0xff, 0x00, 0x00};
+	std::array<std::pair<std::string, std::array<uint8_t, 3>>, 1> layout = { std::make_pair("Esc", colours) };
+	setCustomLayout(layout);
+	std::cin >> a;
+
+	setActiveProfile(1);
+
+	// Finalize the hidapi library
+	hid_exit();
+	return 0;
+}
+
+hid_device* openKeyboard() {
 	std::string hid_path;
 
 	struct hid_device_info* devs, * cur_dev;
-	devs = hid_enumerate(0x0c45, 0x652f);
+	devs = hid_enumerate(0x0c45, 0x652f); //scan for devices matching VID and PID
 	cur_dev = devs;
 	//cycle through all detected HIDs
 	while (cur_dev) {
 
-		if (cur_dev->usage == 0x92) {
+		if (cur_dev->usage == 0x92) { //RGB control interface
 			hid_path = cur_dev->path;
 		}
 
@@ -27,74 +46,97 @@ int main()
 	hid_free_enumeration(devs);
 
 	if (hid_path.empty()) {
-		printf("Could not find device.");
-		return 1;
+		printf("Could not find device.\n");
+		return nullptr;
 	}
 
-	hid_device* handle;
 	// Open the device using the path
+	hid_device* handle;
 	handle = hid_open_path(hid_path.c_str());
 	if (!handle) {
-		printf("unable to open device\n");
+		printf("Unable to open device.\n");
+		return nullptr;
+	}
+
+	return handle;
+}
+
+int writeToKeyboard(hid_device *handle, uint8_t buf[], int length) {
+
+	//write buf data to device at handle
+	int res = hid_write(handle, buf, length);
+	if (res < 0) {
+		printf("Unable to write\n");
+		printf("Error: %ls\n", hid_error(handle));
+	}
+
+	return res;
+}
+
+void setCustomLayout(std::array<std::pair<std::string, std::array<uint8_t, 3>>, 1> layout) {
+	int res;
+
+	hid_device* handle = openKeyboard();
+	if (!handle) {
+		return;
+	}
+
+	uint8_t buf[64];
+	std::copy(std::begin(data_settings), std::end(data_settings), std::begin(buf));
+
+	res = writeToKeyboard(handle, data_start, 64); //tell device this is start of data
+
+	for (auto element :layout) { //for every key config in layout
+		// search keycode map for key identifier
+		auto keyID = map_keycodes.at(element.first);
+		// set keycode values
+		buf[1] = keyID[0];
+		buf[5] = keyID[1];
+		buf[6] = keyID[2];
+		// set colour values
+		buf[8] = element.second[0];
+		buf[9] = element.second[1];
+		buf[10] = element.second[2];
+
+		// write key config to device
+		res = writeToKeyboard(handle, buf, 64);
+	}
+
+	res = writeToKeyboard(handle, data_end, 64); //tell device this end start of data
+
+	hid_close(handle);
+}
+
+int setActiveProfile(int profile) {
+
+	int res;
+	hid_device* handle = openKeyboard();
+	if (!handle) {
 		return 1;
 	}
 
-	// ---
-	// prepare data packets
-	//uint8_t buf[64];
-	//std::copy(std::begin(data_profile), std::end(data_profile), std::begin(buf));
-
-	//int profile;
-	//std::cin >> profile;
-	//// change data
-	//if (profile == 1) {
-	//	//do nothing
-	//}
-	//else if (profile == 2) {
-	//	buf[1] = 0xe1;
-	//	buf[18] = 0x01;
-	//}
-	//else if (profile == 3) {
-	//	buf[1] = 0xe2;
-	//	buf[18] = 0x02;
-	//}
-	//else {
-	//	std::cout << "Invalid input.\n";
-	//	return 1;
-	//}
-
-	//res = hid_write(handle, buf, 64);
-	//if (res < 0) {
-	//	printf("Unable to write()\n");
-	//	printf("Error: %ls\n", hid_error(handle));
-	//}
-	//else {
-	//	printf(":D");
-	//}
-	// ---
 	uint8_t buf[64];
-	std::copy(std::begin(data_settings), std::end(data_settings),	std::begin(buf));
+	std::copy(std::begin(data_profile), std::end(data_profile), std::begin(buf)); //get data signal for switch profile
 
-	res = hid_write(handle, data_start, 64);
+	//change signal to correspond to correct profile
+	if (profile == 1) {
+		//do nothing, 1 is default of data_profile
+	}
+	else if (profile == 2) {
+		buf[1] = 0xe1;
+		buf[18] = 0x01;
+	}
+	else if (profile == 3) {
+		buf[1] = 0xe2;
+		buf[18] = 0x02;
+	}
+	else {
+		std::cout << "Invalid input.\n";
+		return 1;
+	}
 
-	// keycode
-	buf[1] = 0x57;
-	buf[5] = 0x03;
-	buf[6] = 0x00;
-	// color
-	buf[8] = 0xff;
-	buf[9] = 0x00;
-	buf[10] = 0x00;
+	res = writeToKeyboard(handle, buf, 64);
 
-	res = hid_write(handle, buf, 64);
-
-	res = hid_write(handle, data_end, 64);
-	// ---
-
-	// Close the device
 	hid_close(handle);
-
-	// Finalize the hidapi library
-	res = hid_exit();
-	return 0;
+	return res;
 }
--- keymeleon-console/keymeleon-console.h
+++ keymeleon-console/keymeleon-console.h
@@ -1,7 +1,18 @@
 #pragma once
-#include <cstdint>
+#include <iostream>
+#include <string>
+#include <map>
+#include <array>
+#include <hidapi.h>
+
+extern std::map<std::string, std::array<uint8_t, 3>> map_keycodes;
 
 extern uint8_t data_profile[64];
 extern uint8_t data_start[64];
 extern uint8_t data_end[64];
 extern uint8_t data_settings[64];
+
+hid_device* openKeyboard();
+int writeToKeyboard(hid_device* handle, uint8_t buf[], int length);
+void setCustomLayout(std::array<std::pair<std::string, std::array<uint8_t, 3>>, 1> layout);
+int setActiveProfile(int profile);
