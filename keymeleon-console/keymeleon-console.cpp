9329d6802b9d6b140a123b4ff5de42f484c0c5d9
Jack Gillespie <jack02gillespie@gmail.com>
2022-08-08 16:06:05 +0100

    Recolour key demo

--- keymeleon-console/keymeleon-console.cpp
+++ keymeleon-console/keymeleon-console.cpp
@@ -41,36 +41,54 @@ int main()
 
 	// ---
 	// prepare data packets
-	uint8_t buf[64];
-	std::copy(std::begin(data_profile), std::end(data_profile), std::begin(buf));
+	//uint8_t buf[64];
+	//std::copy(std::begin(data_profile), std::end(data_profile), std::begin(buf));
 
-	int profile;
-	std::cin >> profile;
-	// change data
-	if (profile == 1) {
-		//do nothing
-	}
-	else if (profile == 2) {
-		buf[1] = 0xe1;
-		buf[18] = 0x01;
-	}
-	else if (profile == 3) {
-		buf[1] = 0xe2;
-		buf[18] = 0x02;
-	}
-	else {
-		std::cout << "Invalid input.\n";
-		return 1;
-	}
+	//int profile;
+	//std::cin >> profile;
+	//// change data
+	//if (profile == 1) {
+	//	//do nothing
+	//}
+	//else if (profile == 2) {
+	//	buf[1] = 0xe1;
+	//	buf[18] = 0x01;
+	//}
+	//else if (profile == 3) {
+	//	buf[1] = 0xe2;
+	//	buf[18] = 0x02;
+	//}
+	//else {
+	//	std::cout << "Invalid input.\n";
+	//	return 1;
+	//}
+
+	//res = hid_write(handle, buf, 64);
+	//if (res < 0) {
+	//	printf("Unable to write()\n");
+	//	printf("Error: %ls\n", hid_error(handle));
+	//}
+	//else {
+	//	printf(":D");
+	//}
+	// ---
+	uint8_t buf[64];
+	std::copy(std::begin(data_settings), std::end(data_settings),	std::begin(buf));
+
+	res = hid_write(handle, data_start, 64);
+
+	// keycode
+	buf[1] = 0x57;
+	buf[5] = 0x03;
+	buf[6] = 0x00;
+	// color
+	buf[8] = 0xff;
+	buf[9] = 0x00;
+	buf[10] = 0x00;
 
 	res = hid_write(handle, buf, 64);
-	if (res < 0) {
-		printf("Unable to write()\n");
-		printf("Error: %ls\n", hid_error(handle));
-	}
-	else {
-		printf(":D");
-	}
+
+	res = hid_write(handle, data_end, 64);
 	// ---
 
 	// Close the device
--- keymeleon-console/keymeleon-console.h
+++ keymeleon-console/keymeleon-console.h
@@ -2,3 +2,6 @@
 #include <cstdint>
 
 extern uint8_t data_profile[64];
+extern uint8_t data_start[64];
+extern uint8_t data_end[64];
+extern uint8_t data_settings[64];
