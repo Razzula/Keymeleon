a6c867ad9ba422db38c59a62c8b65f52aef99f3f
Jack Gillespie <jack02gillespie@gmail.com>
2022-08-09 11:58:57 +0100

    Monitors current foreground app

--- Keymeleon/MainWindow.xaml.cs
+++ Keymeleon/MainWindow.xaml.cs
@@ -13,16 +13,62 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 namespace Keymeleon
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        NativeMethods.WinEventDelegate winEventProcDelegate;
+        private const uint WINEVENT_OUTOFCONTEXT = 0x0000;
+        private const uint EVENT_SYSTEM_FOREGROUND = 0x0003;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //setup method to handle events (change of focus)
+            winEventProcDelegate = new NativeMethods.WinEventDelegate(WinEventProc);
+            NativeMethods.SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, System.IntPtr.Zero, winEventProcDelegate, (uint)0, (uint)0, WINEVENT_OUTOFCONTEXT); //begin listening to change of window focus
+        }
+
+        private void WinEventProc(System.IntPtr hWinEventHook, uint eventType, System.IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            Debug.WriteLine("Foreground changed");
+
+            //get current focused process
+            IntPtr pid = System.IntPtr.Zero;
+            NativeMethods.GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+
+            //get name of application that process is owned by
+            try
+            {
+                Debug.WriteLine(p.MainModule.FileVersionInfo.FileDescription.ToString());
+            }
+            catch (System.ComponentModel.Win32Exception)
+            // if Keymeleon is x86, this exception will throw when trying to access an x64 program (however, it should only be x86 on 32bit CPUs, so there shouldn't be any x64 programs to trigger this) //TODO add check for this
+            // if Keymeleon is not elevated, this exception will throw when trying to access an elevated program //TODO request that Keymeleon be elevated
+            {
+                Debug.WriteLine(p.MainWindowTitle.ToString());
+            }
+            catch (NullReferenceException)
+            {
+                Debug.WriteLine("nullptr error");
+            }
+        }
+
+        static class NativeMethods
+        {
+
+            [DllImport("user32.dll")]
+            public static extern System.IntPtr SetWinEventHook(uint eventMin, uint eventMax, System.IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+            public delegate void WinEventDelegate(System.IntPtr hWinEventHook, uint eventType, System.IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+            [DllImport("user32.dll")]
+            public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out IntPtr ProcessId);
         }
     }
 }
